fefea67f1590d56e04a1cd77d6d7137ed5e5901c
Kinan wehba <Kinan.itm@gmail.com>
2023-07-26 23:26:48 +0300

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   event/__pycache__/views.cpython-311.pyc
    	new file:   event/templates/event/event_detail.html
    	new file:   event/templates/event/event_weekly.html
    	deleted:    event/templates/event/home.html
    	modified:   event/views.py

--- event/views.py
+++ event/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
 def event_weekly(request):
-    pass
+    event_weekly= Event.objects.all()
+
+    context={"event_weekly":event_weekly}
+    return render(request, "event/event_weekly.html",context)
 
 def event_detail(request,id):
-    pass
+    event_detail = Event.objects.get(id=id)
+    context = {'event_detail':event_detail}
+    return render(request,'event/event_detail.html',context)
